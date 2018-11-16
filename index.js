41e6e1e0afa31730c4ba7a38a4c0937d0a05aff2
Maxiux <storresp85@elpuig.xeill.net>
2018-11-16 16:39:20 +0100

    añadida pepa

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 alert("COMORL???")
 alert("TE DA CUEN???")
+alert("ERE UN FISTRO DE PECADOR!!!")
 document.getElementById("superboton").addEventListener("click", () => document.getElementById("superimagen").style.display='none');
